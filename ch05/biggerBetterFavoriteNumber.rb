b0d93e1300e02d8dc81bc5f4789d33f2d0dd4f0a
SackIt <samaryala@gmail.com>
2017-06-02 23:59:09 -0500

    Chapter 5 assignment

---
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,6 @@
+puts 'Hello! What\'s your favorite number?'
+fav = gets.chomp
+better = fav.to_i + 1
+puts 'Ah, so your favorite number is ' + fav + '? That\'s an awesome number,
+but what do you think about ' + better.to_s + '? It\'s an even bigger number,
+so it would make an even better favorite, don\'t you think?'
---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts 'Hello! What\'s your first name?'
+first = gets.chomp
+puts 'What about your middle name?'
+middle = gets.chomp
+puts 'And last but not least, your last name?'
+last = gets.chomp
+puts 'Your name is ' + first + ' ' + middle + ' ' + last + '? That\'s a wonderful name!'
