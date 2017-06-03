5e2b1417d128e6cfc9bfaebe1d0aa0d6d14b7980
SackIt <samaryala@gmail.com>
2017-06-02 22:38:14 -0500

    updated files to account for leap years

--- ch2/minutesInDecade.rb
+++ ch2/minutesInDecade.rb
@@ -1,1 +1,4 @@
-puts 60*24*365*10
+puts 'a decade can have 2 or 3 leap years: if it has two, it has this many minutes'
+puts 60*24*365*8+60*24*366*2
+puts 'if it has 3, it has this many minutes'
+puts 60*24*365*7+60*24*366*3
